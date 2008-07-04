10cd6fd9e94472fa67005097b4bd3e6b64cab254
quan <quan@8a51e1d0-382e-4e9e-8ee3-68392b4c822a>
2008-07-04 19:16:57 +0000

    svn restructured

---
+++ pnlang/deu/contenttypesapi.php
@@ -0,0 +1,6 @@
+<?php
+define('_MYMAP_CONTENTENTTYPE_SHOWMAPTITLE',	'Grafische Landkarte (MyMap)');
+define('_MYMAP_CONTENTENTTYPE_SHOWMAPDESCR',	'Eine Landkarte mit Markern und / oder Wegpunkten in diese Seite einfügen. Die einzufügende Karte muss allerdings schon in MyMap angelegt sein!');
+define('_MYMAP_CONTENTTYPE_NOMIDFOUND',			'Die Karte mit der übergebenen ID-Nummer kann nicht gefunden und somit nicht angezeigt werden.');
+define('_MYMAPHOOKDISPLAYERROR',				'Es ist ein Fehler während des Versuchs, eine Karte einzubinden, aufgetreten!');
+?>
