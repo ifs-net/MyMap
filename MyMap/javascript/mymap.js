5ebb58074ef66181c0a3de047116b35a7c2a0db1
quan <quan@8a51e1d0-382e-4e9e-8ee3-68392b4c822a>
2008-11-20 22:29:46 +0000

    added base url in javascript

--- MyMap/javascript/mymap.js
+++ MyMap/javascript/mymap.js
@@ -3,13 +3,13 @@ function toggledisplay (id, indicator){
 		var obj = document.getElementById(id);
 		var pic = document.getElementsByName(indicator);
 		obj.style.display = (obj.style.display=='block'?'none':'block');
-		pic[0].src = (obj.style.display=='block'?'modules/MyMap/pnimages/hide.gif':'modules/MyMap/pnimages/show.gif');
+		pic[0].src = (obj.style.display=='block'?document.location.pnbaseURL + 'modules/MyMap/pnimages/hide.gif':document.location.pnbaseURL + 'modules/MyMap/pnimages/show.gif');
 	} else if(document.all) { 		// Internet Exploder
 		id.style.display = (id.style.display=='block'?'none':'block');
-		indicator.src = (id.style.display=='block'?'modules/MyMap/pnimages/hide.gif':'modules/MyMap/pnimages/show.gif');
+		indicator.src = (id.style.display=='block'?document.location.pnbaseURL + 'modules/MyMap/pnimages/hide.gif':document.location.pnbaseURL + 'modules/MyMap/pnimages/show.gif');
 	} else if (document.layers) { 	// Netscape 4.x
 		document.id.style.display = (document.id.style.display=='block'?'none':'block');
-		document.indicator.src = (document.id.style.display=='block'?'modules/MyMap/pnimages/hide.gif':'modules/MyMap/pnimages/show.gif');
+		document.indicator.src = (document.id.style.display=='block'?document.location.pnbaseURL + 'modules/MyMap/pnimages/hide.gif':document.location.pnbaseURL + 'modules/MyMap/pnimages/show.gif');
 	}
 }
 
