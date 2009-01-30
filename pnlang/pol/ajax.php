0a0f21a0cf666d47389a63f1eb650a2a8c96aaf3
quan <quan@8a51e1d0-382e-4e9e-8ee3-68392b4c822a>
2009-01-30 21:49:19 +0000

    included polish language from cozi download

---
+++ pnlang/pol/ajax.php
@@ -0,0 +1,16 @@
+<?php
+// overview table in user-display
+define('_MYMAPEDIT',     'modyfikuj');
+define('_MYMAPDELETE',     'usuń');
+define('_MYMAPTITLE',     'Tytuł');
+define('_MYMAPPOSTALCODE',    'Kod pocztow');
+define('_MYMAPSHORTPLACENAME',   'Nazwa');
+define('_MYMAPSHORTLNG',    'Długość geog.');
+define('_MYMAPSHORTLAT',    'Szerokość geog.');
+define('_MYMAPSHORTUID',    'Autor');
+define('_MYMAPSHORTDATE',    'Data');
+define('_MYMAPREALLYDELETE',   'Czy na pewno chcesz usunąć ten punkt?');
+// map export
+define('_MYMAPEXPORTCOPYANDPASTE',  'Zaznacz cały tekst w polu tekstowym poniżej i zapisz go jako plik .gpx. Możesz zaimportować ten plik do dowolego progamu.');
+define('_MYMAPCLOSEWINDOW',    'Zamkij okno');
+?>
---
+++ pnlang/pol/user.php
@@ -0,0 +1,114 @@
+<?php
+// account settings panel
+define('_MYMAPMYMAPS',     'Moje mapy i drogi');
+// needle / code part
+define('_MYMAPNOSCRIPTMESSAGE',   'Proszę włącz javascript aby zobaczyć mapę która powinna być wyświetlona tutaj');
+define('_MYMAPMAPNOTFOUND',    'Mapa o podanym ID nie została znaleziona');
+define('_MYMAPHOOKREGISTERFAILED',  'Błąd inizjalizacji haka');
+define('_MYMAPHOOKDELETEFAILED',  'Błąd usuwania haka');
+define('_MYMAPHOOKDISPLAYERROR',  'Błąd usuwania haka dla haków MyMap');
+define('_MYMAPMAPERROR',    'Błąd MyMap');
+define('_MYMAPMAPIDNOTFOUND',   'Nie znaleziono mapy o podanym ID');
+define('_MYMAPEXPLAINHOOKERR',   'Co to oznacza? Autor tej strony chciał dołączyć tutaj graficzną mapę, ale mapa która powinna być dołączona nie istnieje w bazie danych. Jeśli jesteś właścicielem tej strony sprawdź watrość ID mapy lub skontaktuj się z administratoem portallu jeśli nie wiesz jak rozwiązać ten problem. Jeśli jesteś "tylko" zwykłym odwiedzającym po prostu zignoruj ten błąd lub napisz miłego maila to właściciela tej strony :-)');
+// main
+define('_MYMAPWELCOMETOMYMAP',   'MyMap - utwórz mapę z markerami');
+define('_MYMAPEXPLAINMYMAP',   'Tym modułem możesz tworzyć mapy i umieszczać na nich markery. Tak więc możesz pokazać swój dom, miejsce spotkania, itp.');
+define('_MYMAPADDNEWMAP',    'Utwórz nową mapę');
+define('_MYMAPMANAGEMENT',     'Zarządzanie mapą');
+define('_MYMAPMANAGEMAPS',    'Możesz zarządać swoimi mapami tutaj (usuwać lub edytować istniejące mapy)');
+define('_MYMAPNOMAP',     'Nie ma mapy przypisanej do Twojego konta');
+define('_MYMAPEDIT',     'modyfikuj');
+define('_MYMAPDELETE',     'usuń');
+define('_MYMAPSHOW',     'pokaż / dodaj markery lub punkty drogi');
+// routes
+define('_MYMAPROUTEMANAGMENT',   'Zarządzanie drogą / punktami drogi');
+define('_MYMAPROUTESINTRO',    'Możesz dodać drogę do mapy. Pownieneś zdefiniować punkty drogi na mapie, wtedy zostanie narysowana lini prowadząca między nimi.');
+define('_MYMAPADDCOORDINATE',   'dodaj');
+define('_MYMAPORIMPORT',    'lub użyj funkcji importu (aby zaimportować, po wybraniu pliku, kliknij guzik dodawania)');
+define('_MYMAPIMPORTERROR',    'Wystąpił błąd w czasie importu pliku');
+define('_MYMAPIMPORTSUCCESSFULL',  'Plik został zaimportowany');
+define('_MYMAPNUMSOFWPS',    'Liczba zapisanych punktów drogi dla mapy');
+define('_MYMAPWAYPOINTADDED',   'Poprawnie dodano punkt drogi');
+define('_MYMAPWAYPOINTSDELETED',  'Wszystkie punkty drogi dla tej mapy zostały poprawnie usunięte!');
+define('_MYMAPWAYPOINTSDELETIONERROR', 'Błąd w czasie usuwanie punktu drogi');
+define('_MYMAPWAYPOINTDELETED',   'Poprawnie usunięto punkt drogi');
+define('_MYMAPDELETEALLWAYPOINTS',  'Usuń wszystkie punkty drogi');
+define('_MYMAPDELETELASTWAYPOINT',  'Usuń ostatni punkt drogi');
+define('_MYMAPACTIONS',     'Akcje');
+define('_MYMAPBACKTOMAP',    'Powrót do zarządzania markerami mapy');
+define('_MYMPUPLOADERROR',    'Błąd ładowani pliku, lub plik nie zawierał danych');
+define('_MYMAPORADDWAYPOINT',   'lub dodaj punkt drogi');
+// edit
+define('_MYMAPTITLE',     'Tytuł');
+define('_MYMAPDESCRIPTION',    'Opis');
+define('_MYMAPWIDTH',     'Szerokość (w punktach) mapy');
+define('_MYMAPHEIGHT',     'Height (w punktach) mapy');
+define('_MYMAPZOOMFACTOR',    'Powiększenie (1 - cała mapa) mapy');
+define('_MYMAPWIKI',     'Pozwól innym użytkowmikom dodawać ich markery do Twojej mapy');
+define('_MYMAPYES',      'Tak');
+define('_MYMAPNO',      'Nie');
+define('_MYMAPHYBRID',     'hybrydowa');
+define('_MYMAPNORMAL',     'normalna');
+define('_MYMAPSATELLITE',    'satelitarna');
+define('_MYMAPMAPTYPE',     'Jak wyświetlać mapę');
+define('_MYMAPOPTIONALTABLE',   'Wyświetlaj obok mapy tabelę z listą wszystkich markerów');
+define('_MYMAPUPDATECREATEMAP',   'Uaktualnij lub utwórz mapę');
+define('_MYMAPRETURNTOMAINPAGE',  'Powrót do strony głównej');
+define('_MYMAPUPDATED',     'Mapa została poprawnie zaktualizowana');
+define('_MYMAPDELETEMAP',    'Usuń mapę i wszystkie markery');
+define('_MYMAPMAPCREATED',    'Nowa mapa została poprawnie utworzona');
+define('_MYMAPMAPDELETED',    'Mapa została poprawnie usunięta');
+define('_MYMAPMAPDELETIONERROR',  'Wystąpił błąd w czasie usuwania mapy');
+// display
+define('_MYMAPAUTHOR',     'Autor tej mapy');
+define('_MYMAPMAPNOTFOUND',    'Zaznaczona mapa nie istnieje');
+define('_MYMAPADDPOINTS',    'Wstaw nowy element');
+define('_MYMAPADDPOINTSEXPL',   'Możesz dodawać markety do tej mapy. Jako opisu możesz także używać HHTMLu. Może to pomódz w wstawianiu materiału video do mapy. Jeśli nie znasz dokładych współrzędnych położenia markera wypełnij pola kodu krajowego i pocztowego. Poszukamy wtedy współrzędnych dla Ciebie. Uwaga: Możesz także kliknąć na punkt na mapie. Współrzędne klikniętego punktu będą pokazane w poniższym formularzu! Tak że jest to bardzo proste, nieprawdaż?');
+define('_MYMAPCOUNTRYCODE',    '2 literowy kod kraju');
+define('_MYMAPPOSTALCODE',    'Kod pocztowy');
+define('_MYMAPROUTEMANAGEMENT',   'Zarządzanie drogą / punktami drogi');
+define('_MYMAPLAT',      'Szerokość geograficzna');
+define('_MYMAPLNG',      'Długość geograficzna');
+define('_MYMAPPLACENAME',    'Nazwa zaznaczanego miejsca');
+define('_MYMAPDATE',     'Data tego wpisu lub zdarzenia w zaznaczanym miejscu');
+define('_MYMAPUPDATECREATEPOINT',  'aktualizuj / dodaj punkt');
+define('_MYMAPCOORDSMISSING',   'Błędna długość i szerokość geograficzna. Proszę podaj nam kod kraju i kod pocztowy Twojego miejsca. Także nazwa wprowadzanego miejsca może nam pomódz.');
+define('_MYMAPPOINTUPDATED',   'Marker został uaktualniony');
+define('_MYMAPPOINTADDED',    'Marker został dodany');
+define('_MYMAPMULTIFOUND',    'Zostało znalezione więcej niż jedno miejsce:');
+define('_MYMAPCHOOSEONE',    'proszę wybrać kod pocztowy i / lub kod kraju który potrzebujesz');
+define('_MYMAPPOINTSOVERVIEW',   'Przegląd markerów');
+define('_MYMAPPOINTSOVERVIEWEXPL',  'Jeśli jesteś właścicielem lub masz autoryzację możesz tutaj zarządać markerami!');
+define('_MYMAPSHOWOVERVIEW',   'Pokaż listę wszystkich markerów');
+define('_MYMAPSHOWADDPOINTS',   'Dodaj nowe markery');
+define('_MYMAPPOINTDELETED',   'Zaznaczony punkt został poprawnie usunięty');
+define('_MYMAPPOINTDELETIONERROR',  'Wystąpił błąd w czasie proby usunięcia zaznaczonego punktu');
+define('_MYMAPINSERTMAP',    'Czy chcesz udostępnić tą mapę');
+define('_MYMAPINSERTMAPHOWTO',   'Jeśli chcesz wstawić tą mapę do wypowiedzi na forum lub do wpisu bloga po prostu wstaw poniższy kod jako czysty tekst');
+define('_MYMAPNOTIFYSENT',    'Autor mapy został powiadominy mailem o tym, że dodałeś nowy markter');
+define('_MYMAPFULLSCREENMODE',   'Tryb pełnoekranowy');
+define('_MYMAPMINIMIZE',    'Minimalizuj');
+define('_MYMAPREALLYDELETE',   'Czy na pewno chcesz usunąć ten punkt?');
+// map control (hook, display, ...)
+define('_MYMAPCLOSEMAPCONTROL',   'zamknij menu');
+define('_MYMAPHIDEMARKERS',    'ukryj markery');
+define('_MYMAPSHOWMARKERS',    'pokaż markery');
+define('_MYMAPHIDEROUTES',    'ukryj drogę');
+define('_MYMAPSHOWROUTES',    'pokaż drogę');
+define('_MYMAPEXPORTGPX',    'eksportuj gpx');
+define('_MYMAPEXPORTKML',    'eksportuj kml');
+define('_MYMAPUNKNOWNTYPE',    'Nieznany tym eksportu');
+define('_MYMAPWAITFOREXPORT',   'Proszę poczeć aż dane do eksportu zostaną wygenerowane!');
+// notification email
+define('_MYMAPNOTIFYINTRO',    'To jest e-mail z powiadomieniem do Ciebie. Jesteś właścicielem mapy, a ktoś inny z twojej społeczności dodał nowy marker do Twojej mapy. Jeśli nie chcesz aby inni użytkownicy mogli dodawać punkty do twojej mapy zmień ustawienia tej mapy!');
+define('_MYMAPNOTIFYAFFECTEDMAP',  'Informacje o mapie');
+define('_MYMAPMAPID',     'Wewnętrzny numer ID mapy');
+define('_MYMAPMAPTITLEOFYOURMAP',  'Tytuł Twojej mapy');
+define('_MYMAPNEWMARKERTITLE',   'Tytuł nowego markera');
+define('_MYMAPNEWMARKERTEXT',   'Dodatkowy tekst dla markera');
+define('_MYMAPADDEDBY',     'Marker zostal dodany przez');
+define('_MYMAPMAPLINK',     'Zobacz mapę');
+define('_MYMAPDONOTREPLY',    'Proszę nie odpowiadaj na tego maila! To jest tylko automatycznie wygenerowany tekst...');
+define('_MYMAPUSERID',     'ID użytkownika');
+define('_MYMAPSUBJECT',     'Twoja mapa została zmodyfikowana');
+?>
