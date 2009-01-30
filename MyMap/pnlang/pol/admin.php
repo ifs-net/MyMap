9e9188bd0085e85a7069fab3456fbe7b9feffd24
quan <quan@8a51e1d0-382e-4e9e-8ee3-68392b4c822a>
2009-01-30 21:49:19 +0000

    included polish language from cozi download

---
+++ MyMap/pnlang/pol/admin.php
@@ -0,0 +1,33 @@
+<?php
+// main
+define('_MYMAPMAINCONFIG',    'Konfiguracja zaplecza MyMap');
+define('_MYMAPMAINTITLE',    'Interfejs administracyjn MyMap');
+define('_MYMAPADMININTRO',    'Aby używać modułu MyMap muszisz zarejestrować się przynajmniej u jednego z podanych dostawców. Proszę zapisać tutaj klucz API (ang. API key) który został wygenerowany przez dostawcę dla Twojego portalu!');
+define('_MYMAPPROVIDER',    'Proszę wybrać dostawcę którego rozwiązanie chcesz używać na Twoim portalu. Ostatecznie kluch od dostawcy powinien być zapisany tutaj!');
+define('_MYMAPGOOGLE',     'Google');
+define('_MYMAPYAHOO',     'yahoo!');
+define('_MYMAPOSM',      'OpenStreetMap');
+define('_MYMAPMAPQUEST',    'MapQuest');
+define('_MYMAPMS',      'Microsoft');
+define('_MYMAPFREEEARTH',    'FreeEarth');
+define('_MYMAPKEYMAP24',    'Map24 application key');
+define('_MYMAPKEYMAPQUEST',    'MapQuest Key');
+define('_MYMAPKEYMULTIMAP',    'MultiMap Key');
+define('_MYMAPMULTIMAP',    'MultiMap');
+define('_MYMAPMAP24',     'Map24');
+define('_MYMAPOPENLAYERS',    'OpenLayers');
+define('_MYMAPKEYGOOGLE',    'Klucz API (ang. API key) do używania z Google Maps');
+define('_MYMAPKEYOSM',     'Klucz API (ang. API key) do używania z OpenStreetMap');
+define('_MYMAPKEYYAHOO',    'ID aplikacji Yahoo\'s');
+define('_MYMAPKEYMS',     'Klucz API (ang. API key) do używania z Microsoft Maps');
+define('_MYMAPUPDATECONFIG',   'aktualizuj konfigurację');
+define('_MYMAPSHOWOVERVIEWMAP',   'Pokaż małą mapę poglądową wewnątrz wyświetlanej mapy');
+define('_MYMAPCONFIGUPDATED',   'Uaktualnienie konfiguracji zakończylo się sukcesem');
+define('_MYMAPGPSBABELPATH',   'Ścieżka do programu GPSBabel (zawierająca binaria / plik wykonywalny! Wprowadź ścieżkę pełną lub relatywną. Zacznij od kłownego katalogu Zikuli jeśli chcesz użyć ścieżki relatywnej!). Ten program może zoptymalizować import danych z pliku GPX.');
+define('_MYMAPGPSBABELERROR',   'Błąd punku drogi w czasie importu pliku GPX');
+define('_MYMAPNOTIFYOWNER',    'Powiadom właściciela mapy ilekroć inna osoba doda marker do innej mapy');
+define('_MYMAPSUPPORTMYMAP',   'Wspomóż moduł MyMap');
+define('_MYMAPDONATETHIS',    'Datek przez PayPal!');
+define('_MYMAPDONATE',     'Lubisz ten moduł i jest on użyteczny dla Ciebie? Prosimy o mały datek dla programistów i z góry za niego dziękujemy! OK - ja bardzo lubię programować i ten moduł jest wolny i ma otwarte źródła - ale jeśli wydasz trochę pieniędzy Wyjdę na miłą kolację przy świecach z moją dziewczyną i będzie mi dużo łatwiej zdobyć w przyszłości wolny czs na programowanie ;-)');
+define('_MYMAPUSEWYSIWYG',    'Użyj edyora WYSIWYG scribite\'s opisów markerów');
+?>
